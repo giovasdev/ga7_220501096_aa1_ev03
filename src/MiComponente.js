0a0548abe43d435e1a623d32eaf7bded8234619b
Giovanny Vasco <maniagva@hotmail.com>
2025-04-21 19:52:31 -0500

    Creación mi primer componente

---
+++ src/MiComponente.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MiComponente() {
+    return (
+        <div className="mi-componente">
+            <h2>Este es mi primer componente</h2>
+            <p>¡Estoy aprendiendo React!</p>
+        </div>
+    );
+}
+
+export default MiComponente;
